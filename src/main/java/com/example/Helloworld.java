9ad67592c6886ab6c96ac60b42971a793511ea01
Thousi10 <Thousifthousi10@gmail.com>
2024-12-01 01:12:24 +0530

    sonar token added

--- src/main/java/com/example/Helloworld.java
+++ src/main/java/com/example/Helloworld.java
@@ -4,6 +4,8 @@ package com.example;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+	System.out.println("new line added");
+    
     }
 }
 
